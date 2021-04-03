0cdcb098bd58e96b946c0826f8d95c71a1aca54f
unknown <turcios50@gmail.com>
2021-04-02 19:49:31 -0700

    added page routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Biography from './pages/Biography'
 import Tourdates from './pages/Tourdates'
 import Merch from './pages/Merch'
 import News from './pages/News'
+import {Route, BrowserRouter as Router} from "react-router-dom"
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
+        <Route path='/Home' component={home}></Route>
+        <Route path='/Biography' component={Biography}></Route>
+        <Route path='/Tourdates' component={Tourdates}></Route>
+        <Route path='/Merch' component={Merch}></Route>
+        <Route path='/News' component={News}></Route>
        <p>this is for Deacon Hill band</p>
       </header>
     </div>
+    </Router>
   );
 }
 
