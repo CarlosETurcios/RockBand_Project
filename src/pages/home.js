ba957eef7a9b709d5cdb493576386d6e6d181919
CarlosETurcios <turcios50@gmail.com>
2021-04-18 22:10:57 -0700

    added some old vegas pics

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ function Home() {
         <div>
             <h1>Deacon Hill</h1>
             <p>coming soon!</p>
+            <img src="/image/oldbingo.jpg" className="vegas-pic" alt="old-vegas-pic"/>
         </div>
     )
 }
