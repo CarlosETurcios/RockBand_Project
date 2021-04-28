80b016b318bdb3ecaa69db3b828212be1f4781e8
CarlosETurcios <turcios50@gmail.com>
2021-04-27 23:28:02 -0700

    added a gif on the tourdates page

--- src/pages/tourdates.js
+++ src/pages/tourdates.js
@@ -7,6 +7,7 @@ function Tourdates(){
         <div className="tourdates-main-page">
             <h1>Tour Dates</h1>
             <p>coming soon!</p>
+            <iframe src="https://giphy.com/embed/UDjF1zMreMld6" width="480" height="416" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/film-vintage-UDjF1zMreMld6">via GIPHY</a></p>
         </div>
     )
 }
