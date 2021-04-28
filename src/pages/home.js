5aaa214373e2d9818dd2b176fa7aaed7f8ebae31
CarlosETurcios <turcios50@gmail.com>
2021-04-27 23:16:51 -0700

    fixed centering on the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,8 @@
-
+import "../styling/homestyling.css"
 
 function Home() {
     return (
-        <div>
+        <div className="home-main-page">
             <h1>Deacon Hill</h1>
             <p>coming soon!</p>
             <img src="/image/oldbingo.jpg" className="vegas-pic" alt="old-vegas-pic"/>
